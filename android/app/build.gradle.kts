dccdfeb75621ae20b73ff5c624260da3c441efbb
Riad Mashrub Shourov <shourovrm@gmail.com>
2026-09-26 00:24:56 +0600

    chore: bump v1.17.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "bd.sicip.qavisit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.16.1"
+        versionCode = 24
+        versionName = "1.17.0"
     }
 
     buildFeatures {
